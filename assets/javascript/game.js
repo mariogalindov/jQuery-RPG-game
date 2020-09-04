5e8d909ad0ee42ea5c7ff6a41c29cc21a5bb66b6
Mario Galindo <mariogalindo89@gmail.com>
2020-09-04 15:06:25 -0500

    added all assets, finished layout and added characters object

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+// Execute when DOM has fully loaded
+
+$(document).ready(function() {
+
+    // Creating object for the characters
+    var characters = {
+        "Qui-Gon Jinn": {
+            name: "Qui-Gon Jinn",
+            health: 130,
+            attack: 8,
+            imageUrl: "assets/images/qui-gon jinn.jpg",
+            counterAttack: 15
+        },
+        "Darth Sidious": {
+            name: "Darth Sidious",
+            health: 185,
+            attack: 15,
+            imageUrl: "assets/images/darth sidious.jpg"
+            counterAttack: 25
+        },
+        "Yoda": {
+            name: "Yoda", 
+            health: 165,
+            attack: 25,
+            imageUrl: "assets/images/yoda.jpg",
+            counterAttack: 20
+        },
+        "Darth Maul": {
+            name: "Darth Maul",
+            health: 140,
+            attack: 15
+            imageUrl: "assets/images/Darth-Maul-jpg",
+            counterAttack: 12
+        }
+
+    }
+
+
+});
